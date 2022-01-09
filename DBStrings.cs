10292ff3b37f90100ab0d8c6f69c48bd073dec79
Omar Abdelati <42877467+Ammuri@users.noreply.github.com>
2022-01-09 17:00:04 -0500

    Add files via upload

---
+++ DBStrings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wonderdrinks
+{
+    public static class DBStrings
+    {
+        public static string UserDBString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\student\Workspace\wonderdrinks\App_Data\UsersDB.mdf;Integrated Security=True";
+            }
+        }
+        public static string DrinksDBString
+        {
+            get
+            {
+                return @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\student\Workspace\wonderdrinks\App_Data\DrinkDB.mdf;Integrated Security=True";
+            }
+        }
+    }
+}
